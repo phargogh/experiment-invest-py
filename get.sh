07f740516325ddb4f9ee0a0678f851e6b3fc364c
James Douglass <jdouglass@stanford.edu>
2015-06-03 10:28:29 -0700

    Adding bash to print a list of pkgs that might need to be installed
    These packages are those that are not in the python2 stdlib but still
    imported by the various packages.

--- get.sh
+++ get.sh
@@ -17,3 +17,25 @@ find natcap/invest -name "*.pyc" | xargs rm
 find natcap/invest -name "*.c" | xargs rm
 find natcap/invest -name "*.cpp" | xargs rm
 
+# find imports that are not in python's stdlib.
+ENV=invest_test_env
+virtualenv --no-setuptools --clear $ENV
+source $ENV/bin/activate
+which python
+echo "Packages imported by python packages that might need to be installed"
+for pkg_name in `grep -rh import\  natcap/invest | sort | uniq | grep -oEi '(^import [a-zA-Z0-9_]+)|(^from [a-zA-Z0-9_]+)' | grep -oE '[^ ]+$' | uniq`
+do
+    python -c "import $pkg_name" &> /dev/null
+
+    # if the package does not import (exit code is nonzero), package might need to be installed.
+    if [ "$?" -ne "0" ]
+    then
+        # If we can find a file by this name in the invest source tree, we won't need to install it to the system
+        if [ ! -e "`find . -name \"$pkg_name.py\" -o -name \"$pkg_name.pyx\"`" ]
+        then
+            echo $pkg_name
+        fi
+    fi
+done
+    
+
